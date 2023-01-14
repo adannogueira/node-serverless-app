e557fe87d222d91dd522bc571aac0043b939df9c
Adan Nogueira <adan.nogueira@gmail.com>
2023-01-14 16:42:38 -0300

    feat: complete implementation of create function

--- app/createTodo.ts
+++ app/createTodo.ts
@@ -1,8 +1,35 @@
+import AWS from "aws-sdk"
 import { Handler } from 'aws-lambda'
+import { v4 as uuid } from 'uuid'
+import { TodoParams } from './interfaces/TodoParams';
+import { Response } from './interfaces/Response';
 
-export const createTodo: Handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello' })
+const TODO_TABLE = process.env.TODO_TABLE;
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
+
+export const createTodo: Handler = async (event: any): Promise<Response> => {
+  const { todo } = JSON.parse(event.body)
+  if (typeof todo !== 'string') return { statusCode: 400, body: "Invalid todo value" }
+  const params = buildParams(todo)
+  const result = await createDynamoData(params)
+  return result
+}
+
+const buildParams = (todo: string): TodoParams => ({
+  TableName: TODO_TABLE,
+  Item: {
+    id: uuid(),
+    todo,
+    checked: false,
+    createdAt: new Date().toDateString(),
+    updatedAt: new Date().toDateString()
   }
+})
+
+const createDynamoData = async (params: TodoParams): Promise<Response> => {
+  const { $response: response } = await dynamoDbClient.put(params).promise()
+  console.log(response)
+  return response.error
+    ? { statusCode: 500, body: JSON.stringify(response.error) }
+    : { statusCode: 201, body: 'Todo created successfully' }
 }
