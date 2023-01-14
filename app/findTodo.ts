4861fbd6a3041a5e882aa38f8e77920fe3d9f286
Adan Nogueira <adan.nogueira@gmail.com>
2023-01-14 18:06:20 -0300

    refactor: group content into a single function

--- app/findTodo.ts
+++ app/findTodo.ts
@@ -1,6 +1,5 @@
 import AWS from "aws-sdk"
 import { Handler } from 'aws-lambda'
-import { Key } from 'aws-sdk/clients/dynamodb'
 import { Response } from './interfaces/Response'
 
 const TODO_TABLE = process.env.TODO_TABLE;
@@ -9,11 +8,6 @@ const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
 export const findTodo: Handler = async ({
   pathParameters: { id } }: any
 ): Promise<Response> => {
-  const rawResult = await findTodoById(id)
-  return rawResult
-}
-
-const findTodoById = async (id: Key): Promise<Response> => {
   const {
     $response: response,
     Item: todo
