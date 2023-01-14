2552a0f40db1de6bf83b7ef561f97f9dbff45566
Adan Nogueira <adan.nogueira@gmail.com>
2023-01-14 17:13:23 -0300

    feat: implement delete function

---
+++ app/deleteTodo.ts
@@ -0,0 +1,27 @@
+import AWS from "aws-sdk"
+import { Handler } from 'aws-lambda'
+import { Key } from 'aws-sdk/clients/dynamodb'
+import { Response } from './interfaces/Response'
+
+const TODO_TABLE = process.env.TODO_TABLE;
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
+
+export const deleteTodo: Handler = async ({
+  pathParameters: { id } }: any
+): Promise<Response> => {
+  const rawResult = await findTodoById(id)
+  return rawResult
+}
+
+const findTodoById = async (id: Key): Promise<Response> => {
+  const {
+    $response: response
+  } = await dynamoDbClient.delete({
+    TableName: TODO_TABLE,
+    Key: { id }
+  }).promise()
+  if (response.error) {
+    return { statusCode: 500, body: JSON.stringify({ message: 'Todo not found'}) }
+  }
+  return { statusCode: 200, body: JSON.stringify({ message: 'Todo deleted successfully' }) }
+}
