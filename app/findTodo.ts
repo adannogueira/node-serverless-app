0325ecbb935aa46808c38aebeaa8e758c214f9b3
Adan Nogueira <adan.nogueira@gmail.com>
2023-01-14 17:10:43 -0300

    feat: add not found parsing

--- app/findTodo.ts
+++ app/findTodo.ts
@@ -21,8 +21,8 @@ const findTodoById = async (id: Key): Promise<Response> => {
     TableName: TODO_TABLE,
     Key: { id }
   }).promise()
-  if (response.error) {
-    return { statusCode: 500, body: JSON.stringify(response.error) }
+  if (response.error || !todo) {
+    return { statusCode: 404, body: JSON.stringify({ message: 'Todo not found'}) }
   }
   return { statusCode: 200, body: JSON.stringify(todo) }
 }
