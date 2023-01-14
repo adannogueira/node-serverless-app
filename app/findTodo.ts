f95244788f71c754b9cb57b1281051b0fa0718fa
Adan Nogueira <adan.nogueira@gmail.com>
2023-01-14 17:08:12 -0300

    feat: implement find function

---
+++ app/findTodo.ts
@@ -0,0 +1,28 @@
+import AWS from "aws-sdk"
+import { Handler } from 'aws-lambda'
+import { Key } from 'aws-sdk/clients/dynamodb'
+import { Response } from './interfaces/Response'
+
+const TODO_TABLE = process.env.TODO_TABLE;
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
+
+export const findTodo: Handler = async ({
+  pathParameters: { id } }: any
+): Promise<Response> => {
+  const rawResult = await findTodoById(id)
+  return rawResult
+}
+
+const findTodoById = async (id: Key): Promise<Response> => {
+  const {
+    $response: response,
+    Item: todo
+  } = await dynamoDbClient.get({
+    TableName: TODO_TABLE,
+    Key: { id }
+  }).promise()
+  if (response.error) {
+    return { statusCode: 500, body: JSON.stringify(response.error) }
+  }
+  return { statusCode: 200, body: JSON.stringify(todo) }
+}
